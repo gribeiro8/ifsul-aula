4234ff11646434f761bcbe4018884d46daf59214
Gabriel Ribeiro <gribeiro8@gmail.com>
2018-11-14 21:58:18 -0200

    Pasta Bower
    Coloquei de volta a pasta bower.

--- config/server.js
+++ config/server.js
@@ -1,27 +1,33 @@
 var express = require('express'); //chamando o modulo
 var consign = require('consign');
 var body = require('body-parser');
-const nunjucks = require("nunjucks"); 
+const nunjucks = require("nunjucks");
 var path = require('path');
 
 
 var app = express();
 
 nunjucks.configure('app/views', {
-   autoescape: true,
-   express: app
- });
- 
- app.use('/', express.static('./public'));
- app.use(express.urlencoded({ extended: false }));
-app.use(body.urlencoded({extended:true}));
+  autoescape: true,
+  express: app
+});
+
+app.use('/', express.static('./public'));
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(body.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-consign({cwd:'app'})
-.include('controllers')
-.then('routes')
-.then('models')
-.into(app);
+consign({
+    cwd: 'app'
+  })
+  .include('controllers')
+  .then('routes')
+  .then('models')
+  .into(app);
 
 module.exports = app;
